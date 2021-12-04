661b37dc457e1eb2c0fb6e6ecfa9386f1135965b
Матвиенко Максим <matvienko@skbkontur.ru>
2021-12-04 22:50:35 +0500

    refactor

--- day-04/src/main.py
+++ day-04/src/main.py
@@ -3,8 +3,12 @@ from collections import Counter
 bingoNum = []
 arrayOfCard = []
 card = []
+won_card = []
+won_num = []
+
 with open("day-04\data\input.txt", mode="r") as input:
     for row in input:
+        row.replace("  ", " ")
         if not bingoNum:
             bingoNum = list(map(int, row.strip().split(sep=",")))
             continue
@@ -17,30 +21,8 @@ with open("day-04\data\input.txt", mode="r") as input:
                 card = []
 
 
-def checkcard(card):
-    for row in card:
-        if len(Counter(row).keys()) == 1:
-            return True
-    matrix = list(zip(*card))
-    for row in matrix:
-        if len(Counter(row).keys()) == 1:
-            return True
-    return False
-
-
-def sumcard(card):
-    s = 0
-    for row in card:
-        for column in row:
-            if not column == "X":
-                s = s + column
-    return s
-
-
-won_card = []
-won_num = []
 for i in bingoNum:
-    for j in range(0, len(arrayOfCard)):
+    for j in range(len(arrayOfCard)):
         card = arrayOfCard[j]
         if card in won_card:
             continue
@@ -48,9 +30,16 @@ for i in bingoNum:
             for col in row:
                 if i == col:
                     row[row.index(col)] = "X"
-        if checkcard(card):
+        if max(
+            len(Counter(a).keys()) == 1 or len(Counter(b).keys()) == 1
+            for a, b in zip(card, zip(*card))
+        ):
             won_card.append(card)
             won_num.append(i)
 
-print(f"Part 1: {won_num[0] * sumcard(won_card[0])}")
-print(f"Part 2: {won_num[-1] * sumcard(won_card[-1])}")
+print(
+    f'Part 1: {won_num[0] * sum([d for x in won_card[0] for d in x if not d == "X"])}'
+)
+print(
+    f'Part 2: {won_num[-1] * sum([d for x in won_card[-1] for d in x if not d == "X"])}'
+)
